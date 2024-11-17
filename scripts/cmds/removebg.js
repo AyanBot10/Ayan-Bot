7b9a9ac7ea45dd8c3b004434a1de9677988868ad
Ayan <ayanboom830@gmail.com>
2024-11-17 18:43:45 +0600

    Add files via upload

---
+++ scripts/cmds/removebg.js
@@ -0,0 +1,44 @@
+const axios = require("axios");
+
+module.exports = {
+  config: {
+    name: "rmvbg",
+    aliases: ["removebg", "rbg"],
+    role: 0,
+    author: "Lawkey Marvellous",
+    countDown: 5,
+    longDescription: "Remove background from images👌.",
+    category: "image",
+    guide: {
+      en: "${pn} reply to an image to remove its background🤜😛."
+    }
+  },
+  onStart: async function ({ message, api, args, event }) {
+    if (!event.messageReply || !event.messageReply.attachments || !event.messageReply.attachments[0]) {
+      return message.reply("Please reply to an image to remove its background 🤦🤦🤦.");
+    }
+
+    const imgurl = encodeURIComponent(event.messageReply.attachments[0].url);
+    const puti = 'xyz';
+    const rbgUrl = `https://smfahim.${puti}/rbg?url=${imgurl}`;
+
+    api.setMessageReaction("⏰", event.messageID, () => {}, true);
+
+    message.reply("🔄| Removing background, please wait...", async (err, info) => {
+      try {
+        const attachment = await global.utils.getStreamFromURL(rbgUrl);
+        message.reply({ 
+          body: `✅| Here is your image with the background removed 🥺🙇🙇🥺:`, 
+          attachment: attachment 
+        });
+
+        let ui = info.messageID;          
+        message.unsend(ui);
+        api.setMessageReaction("✅", event.messageID, () => {}, true);
+      } catch (error) {
+        message.reply("❌| There was an error removing the background from your image.");
+        console.error(error);
+      }
+    });
+  }
+};
